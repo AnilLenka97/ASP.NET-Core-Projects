661391f9723ee20ceacb7d5e7624ee7300cae124
Anil Kumar Lenka <59606027+anil-kumar-lenka@users.noreply.github.com>
2020-05-11 19:05:26 +0530

    Trails Controller Added

--- Parky/ParkyWeb/SD.cs
+++ Parky/ParkyWeb/SD.cs
@@ -3,7 +3,7 @@
     public static class SD
     {
         public static string APIBaseUrl = "https://localhost:44399/";
-        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks/";
         public static string TrailsAPIPath = APIBaseUrl + "api/v1/trails";
     }
 }
